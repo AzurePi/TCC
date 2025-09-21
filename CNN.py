6d14ec91abe44ad739470451cb2a4d377a435b30
azure <ppcardana@hotmail.com>
2025-09-21 12:03:14 -0300

    início

---
+++ CNN.py
@@ -0,0 +1,18 @@
+from transformers import ConvNextFeatureExtractor, ConvNextForImageClassification
+import torch
+from datasets import load_dataset
+
+dataset = load_dataset("huggingface/cats-image")
+image = dataset["test"]["image"][0]
+
+feature_extractor = ConvNextFeatureExtractor.from_pretrained("facebook/convnext-base-224-22k-1k")
+model = ConvNextForImageClassification.from_pretrained("facebook/convnext-base-384-224-1k")
+
+inputs = feature_extractor(image, return_tensors="pt")
+
+with torch.no_grad():
+    logits = model(**inputs).logits
+
+# model predicts one of the 1000 ImageNet classes
+predicted_label = logits.argmax(-1).item()
+print(model.config.id2label[predicted_label]),
